667e5579cfd5fc5ec9a611db960bdb4fb2e4ae69
lucas130919 <cheste1309@gmail.com>
2025-10-10 13:56:40 -0700

    Create .lua

---
+++ .lua
@@ -0,0 +1,31 @@
+local Tabela = {
+	PlayerPermission = {
+		["theking130933"] = 			{Tier = 2000},
+		[""] = 			{Tier = 2000},
+		[""] = 				{Tier = 2000},
+		[""] = 			{Tier = 1},
+		[""] = 			{Tier = 1},
+		[""] = 			{Tier = 1},
+		[""] = 				{Tier = 1},
+		[""] = 			{Tier = 1},
+	},
+	
+	CharacterTier = {
+		["Enrico"] = 					{Tier = 1,Permission = {7237606225}},
+		["DIO"]	=					{Locked = {7237606225}},
+		["Madara"]	=				{Locked = {7237606225}},
+		["Koku"]	=				{Locked = {7237606225}},
+		["Yori"]	=				{Permission = {7237606225}},
+		["Meliodas"]	=				{Permission = {7237606225}},
+		["Goku"]	=				{Locked = {7237606225}},
+		["TOP"]	=					{Locked = {7237606225}},
+		["Giorno"] = 					{Tier = 1050,Permission = {7237606225}},
+		["Hakaishin"] = 				{Locked = {7237606225}},
+		["Boss"] = 					{Locked = {7237606225}},
+		["JP6"] = 					{Tier = 1,Permission = {7237606225}},
+		["Gojo"] = 					{Tier = 1,Permission = {7237606225}}, -- perm temp
+	}
+
+}
+
+return Tabela
