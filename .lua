2062d4ae3960aed3a736f38e05ea989b92cf8b35
lucas130919 <cheste1309@gmail.com>
2026-03-23 11:33:30 -0300

    Atualizar o .lua

--- .lua
+++ .lua
@@ -1,30 +1,30 @@
 local Tabela = {
-	PlayerPermission = {
-		["Zekeyws"] = 			{Tier = 2000},
-		[""] = 			{Tier = 2000},
-		[""] = 				{Tier = 2000},
-		[""] = 			{Tier = 1},
-		[""] = 			{Tier = 1},
-		[""] = 			{Tier = 1},
-		[""] = 				{Tier = 1},
-		[""] = 			{Tier = 1},
-	},
-	
-	CharacterTier = {
-		["Enrico"] = 					{Tier = 1,Permission = {7237606225}},
-		["DIO"]	=					{Locked = {9592003273}},
-		["Madara"]	=				{Locked = {9592003273}},
-		["Koku"]	=				{Locked = {9592003273}},
-		["Yori"]	=				{Permission = {9592003273}},
-		["Meliodas"]	=				{Permission = {9592003273}},
-		["Goku"]	=				{Locked = {9592003273}},
-		["TOP"]	=					{Locked = {9592003273}},
-		["Giorno"] = 					{Tier = 1050,Permission = {9592003273}},
-		["Hakaishin"] = 				{Locked = {9592003273}},
-		["Boss"] = 					{Locked = {9592003273}},
-		["JP6"] = 					{Tier = 1,Permission = {9592003273}},
-		["Gojo"] = 					{Tier = 1,Permission = {9592003273}}, -- perm temp
-	}
+    PlayerPermission = {
+        ["Zekeyws"] =             {Tier = 2000},
+        [""] =             {Tier = 2000},
+        [""] =                 {Tier = 2000},
+        [""] =             {Tier = 1},
+        [""] =             {Tier = 1},
+        [""] =             {Tier = 1},
+        [""] =                 {Tier = 1},
+        [""] =             {Tier = 1},
+    },
+    
+    CharacterTier = {
+        ["Enrico"] =                     {Tier = 1,Permission = {7237606225}},
+                ["DIO"] =                     {Tier = 1,Permission = {9592003273}},
+                ["Madara"] =                     {Tier = 1,Permission = {9592003273}},
+                ["Koku"] =                     {Tier = 1,Permission = {9592003273}},
+        ["Yori"]    =                {Permission = {9592003273}},
+        ["Meliodas"]    =                {Permission = {9592003273}},
+                ["Goku"] =                     {Tier = 1,Permission = {9592003273}},
+                ["TOP"] =                     {Tier = 1,Permission = {9592003273}},
+        ["Giorno"] =                     {Tier = 1050,Permission = {9592003273}},
+                ["Hakaishin"] =                     {Tier = 1,Permission = {9592003273}},
+        ["Boss"] =                     {Locked = {9592003273}},
+        ["JP6"] =                     {Tier = 1,Permission = {9592003273}},
+        ["Gojo"] =                     {Tier = 1,Permission = {9592003273}}, -- perm temp
+    }
 
 }
 
