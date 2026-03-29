19052bcca8360e1c361464315e295b92544592c2
lucas130919 <cheste1309@gmail.com>
2026-03-28 23:32:26 -0300

    Update Hakaishin status from Tier to Locked
    Changed 'Hakaishin' from Tier to Locked status.

--- .lua
+++ .lua
@@ -20,7 +20,7 @@ local Tabela = {
                 ["Goku"] =                     {Tier = 300,Permission = {9592003273}},
                 ["TOP"] =                     {Locked = 1 = {9592003273}},
         ["Giorno"] =                     {Locked = 1050 = {9592003273}},
-                ["Hakaishin"] =                     {Tier = 1,Permission = {9592003273}},
+                ["Hakaishin"] =                     {Locked = {9592003273}},
         ["Boss"] =                     {Locked = {9592003273}},
         ["JP6"] =                     {Tier = 1,Permission = {9592003273}},
         ["Gojo"] =                     {Tier = 1,Permission = {9592003273}}, -- perm temp
