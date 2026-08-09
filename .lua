b952b26d453a87613ade8cebb3b285172e40d211
lucas130919 <cheste1309@gmail.com>
2026-08-09 16:29:47 -0300

    Atualizar o .lua

--- .lua
+++ .lua
@@ -22,7 +22,7 @@ local Tabela = {
 		["Goku"]	=				{Locked = {9592003273}},
 		["TOP"]	=					{Locked = {9592003273}},
 		["Giorno"] = 					{Tier = 1050,Permission = {9592003273,3937637463}},
-		["Hakaishin"] = 				{Permission = {9592003273,10761648167}},
+		["Hakaishin"] = 				{Permission = {9592003273,10761648167,3937637463}},
 		["Boss"] = 					{Locked = {9592003273}},
 		["JP6"] = 					{Tier = 1,Permission = {9592003273,3937637463,11218058268}},
 		["Gojo"] = 					{Tier = 1,Permission = {9592003273,3937637463}}, -- perm temp
